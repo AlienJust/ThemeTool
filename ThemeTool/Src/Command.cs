3a4a9e84252cf3b672ec049c3996a4358bfc2a93
AJ01 <AJ001@mail.ru>
2011-08-24 16:04:21 +0600

    First commit

---
+++ ThemeTool/Src/Command.cs
@@ -0,0 +1,157 @@
+﻿/*
+ * User: AJ01
+ * Date: 24.08.2011
+ * Time: 0:39
+ */
+using System;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using AvalonDock;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ThemeTool
+{
+	public class ToolCommandDefault : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("default");
+			settings.SaveSettings("default");
+		}
+	}
+	public class ToolCommandGeneric : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("generic");
+			settings.SaveSettings("generic");
+		}
+	}
+	
+	public class ToolCommandAutumn : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("#FF800000");
+			settings.SaveSettings("#FF800000");
+		}
+	}
+	public class ToolCommandGrass : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("#FF008000");
+			settings.SaveSettings("#FF008000");
+		}
+	}
+	public class ToolCommandClassic : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("classic");
+			settings.SaveSettings("classic");
+		}
+	}
+	public class ToolCommandAero : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("aero.normalcolor");
+			settings.SaveSettings("aero.normalcolor");
+		}
+	}
+	public class ToolCommandLuna : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("luna.normalcolor");
+			settings.SaveSettings("luna.normalcolor");
+		}
+	}
+	public class ToolCommandDev10 : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("dev2010");
+			settings.SaveSettings("dev2010");
+		}
+	}
+	public class ToolCommandExpressDark : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var settings = new ToolSettings();
+			settings.SetTheme("ExpressionDark");
+			settings.SaveSettings("ExpressionDark");
+		}
+	}
+	public class ToolCommandCustom : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			var colorDialog = new System.Windows.Forms.ColorDialog();
+			var result = colorDialog.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				var color = colorDialog.Color;
+				string clrStr = "#" + color.ToArgb().ToString("X8");
+				var settings = new ToolSettings();
+				settings.SetTheme(clrStr);
+				settings.SaveSettings(clrStr);
+			}
+		}
+	}
+	public class RegisterEventsCommand : AbstractMenuCommand
+	{
+		public BackgroundWorker bw;
+		public override void Run()
+		{
+			bw = new BackgroundWorker();
+			bw.DoWork += delegate
+			{
+				try
+				{
+					while (ICSharpCode.SharpDevelop.Gui.WorkbenchSingleton.MainWindow == null)
+					{
+						Thread.Sleep(100);
+					}
+					ICSharpCode.SharpDevelop.Gui.WorkbenchSingleton.MainWindow.Dispatcher.Invoke
+						(DispatcherPriority.Normal, new System.Threading.ThreadStart
+						 (
+						 	delegate
+						 	{
+						 		string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(RegisterEventsCommand)).Location);
+						 		System.Reflection.Assembly.LoadFrom(System.IO.Path.Combine(path, "AvalonDock.Themes.dll"));
+						 		//string uri = "/AvalonDock.Themes;component/themes/dev2010.xaml";
+						 		//ThemeFactory.ChangeTheme(new Uri(uri, UriKind.RelativeOrAbsolute));
+						 		var settings = new ToolSettings();
+						 		var theme = settings.LoadSettings();
+						 		settings.SetTheme(theme);
+						 	}
+						 )
+						);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.ToString(), "RegisterEventsCommand.Run()");
+				}
+			};
+			bw.RunWorkerAsync();
+		}
+	}
+}
---
+++ ThemeTool/Src/ToolSettings.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: AJ01
+ * Date: 24.08.2011
+ * Time: 9:18
+ */
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+using AvalonDock;
+
+namespace ThemeTool
+{
+	/// <summary>
+	/// Description of ToolSettings.
+	/// </summary>
+	public class ToolSettings
+	{
+		public string SettingsFileName { get { return "ToolThemeSettings.txt"; } }
+		
+		
+		private string AssemblyFolderName { get { return Path.GetDirectoryName(Assembly.GetAssembly(typeof(ToolSettings)).Location); } }
+		
+		
+		public string LoadSettings()
+		{
+			string result = string.Empty;
+			try
+			{
+				using (var sr = new StreamReader(Path.Combine(AssemblyFolderName, SettingsFileName)))
+				{
+					result = sr.ReadLine();
+				}
+			}
+			catch// (Exception ex)
+			{
+				//System.Windows.Forms.MessageBox.Show(ex.ToString());
+			}
+			return result;
+		}
+		
+		public void SaveSettings(string themeName)
+		{
+			try
+			{
+				using (var sw = new StreamWriter(Path.Combine(AssemblyFolderName, SettingsFileName)))
+				{
+					sw.WriteLine(themeName);
+				}
+			}
+			catch// (Exception ex)
+			{
+				//System.Windows.Forms.MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		public void SetTheme(string themeName)
+		{
+			try
+			{
+				if (themeName == "dev2010")
+				{
+					string uri = "/AvalonDock.Themes;component/themes/dev2010.xaml";
+					ThemeFactory.ChangeTheme(new Uri(uri, UriKind.RelativeOrAbsolute));
+				}
+				else if (themeName == "ExpressionDark")
+				{
+					string uri = "/AvalonDock.Themes;component/themes/ExpressionDark.xaml";
+					ThemeFactory.ChangeTheme(new Uri(uri, UriKind.RelativeOrAbsolute));
+				}
+				else if (themeName == "classic" || themeName == "generic" || themeName == "luna.normalcolor" || themeName == "aero.normalcolor")
+				{
+					ThemeFactory.ChangeTheme(themeName);
+				}
+				else if (themeName.StartsWith("#"))
+				{
+					//System.Windows.Forms.MessageBox.Show(themeName);
+					byte a = byte.Parse(themeName.Substring(1, 2), NumberStyles.HexNumber);
+					byte r = byte.Parse(themeName.Substring(3, 2), NumberStyles.HexNumber);
+					byte g = byte.Parse(themeName.Substring(5, 2), NumberStyles.HexNumber);
+					byte b = byte.Parse(themeName.Substring(7, 2), NumberStyles.HexNumber);
+					ThemeFactory.ChangeColors(System.Windows.Media.Color.FromArgb(a, r, g, b));
+				}
+				else if (themeName == "default")
+				{
+					ThemeFactory.ResetTheme();
+				}
+			}
+			catch// (Exception ex)
+			{
+				//System.Windows.Forms.MessageBox.Show(ex.ToString(), "At ToolSettings.SetTheme()");
+			}
+		}
+	}
+}
